527b6206325cab2de7afe2a8de3631524127f066
Rita Moura <ritamoura520@gmail.com>
2023-06-19 14:37:49 -0300

    Teste: classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    vegan_ingredient = Ingredient('tomate')
+    ingredient_animal_derived = Ingredient('presunto')
+    invalid_ingredient = Ingredient('')
+
+    assert vegan_ingredient == vegan_ingredient
+    assert vegan_ingredient != ingredient_animal_derived
+    assert invalid_ingredient.name == ''
+
+    assert vegan_ingredient.restrictions == set()
+
+    assert vegan_ingredient.__hash__() == vegan_ingredient.__hash__()
+    assert vegan_ingredient.__hash__() != ingredient_animal_derived.__hash__()
+
+    assert vegan_ingredient.__repr__() == "Ingredient('tomate')"
